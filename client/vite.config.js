c2f7a7b17f1c117cf1a208a9d8c1c6b83248905e
pruthvi <pruthvirajbarbole55@gmail.com>
2025-03-14 23:56:29 +0530

    n3

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:10000,
+    port:10000, host: '0.0.0.0', 
     proxy:{
       '/api': {
         // target: import.meta.env.REACT_APP_BACKENDURI, // Use the environment variable
