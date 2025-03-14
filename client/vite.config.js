31ba6eb29250c9a533b4dbeea009a006eabd96fc
pruthvi <pruthvirajbarbole55@gmail.com>
2025-03-15 00:21:30 +0530

    jk

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     proxy:{
       '/api': {
         target: 'https://tech-job-portal.onrender.com', // Use the environment variable
-        changeOrigin: false,
+        changeOrigin: true,
       },
     },
   },
