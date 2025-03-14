0320011285997702ac99d960ec6ec393fc9c0f1c
pruthvi <pruthvirajbarbole55@gmail.com>
2025-03-15 00:03:21 +0530

    v

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port:10000, host: '0.0.0.0', allowedHosts: ['https://tech-job-portal.onrender.com','https://tech-job-portal-1.onrender.com/'], 
+    port:10000, host: '0.0.0.0',strictPort: true, allowedHosts: ['https://tech-job-portal-1.onrender.com/'], 
     proxy:{
       '/api': {
         // target: import.meta.env.REACT_APP_BACKENDURI, // Use the environment variable
